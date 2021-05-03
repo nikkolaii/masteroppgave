50e4eb7d937c1e0961115af6a2c1f31513a71755
Nikolai <nikolaiolav@hotmail.com>
2021-05-03 18:48:32 +0200

    commit message 	modified:   ros_detection/CMakeLists.txt 	modified:   ros_detection/launch/ros_detection.launch 	modified:   ros_detection/src/get_tf.cpp 	modified:   ros_detection/src/lookforthis.cpp 	ros_detection/launch/bag.launch 	ros_detection/src/actionclientsim.cpp 	ros_detection/src/distance.cpp 	ros_detection/src/get_tf (copy).cpp

--- ros_detection/src/get_tf.cpp
+++ ros_detection/src/get_tf.cpp
@@ -12,18 +12,21 @@ class PersonBroadcaster
 public:
 PersonBroadcaster(){
   dist_sub_ = nh_.subscribe("/Optimized_person_distance",1, &PersonBroadcaster::callback, this);
-  path_pub_ = nh_.advertise<nav_msgs::Path>("person_path",100, true);
-  move_base_pub_ = nh_.advertise<geometry_msgs::Point>("move_base_goal_topic",100,true);
-  anglePub_ = nh_.advertise<std_msgs::Float64>("FOV_angle", 100, true);
+  path_pub_ = nh_.advertise<nav_msgs::Path>("person_path",1, true);
+  move_base_pub_ = nh_.advertise<geometry_msgs::Point>("move_base_goal_topic",1,true);
+  anglePub_ = nh_.advertise<std_msgs::Float64>("FOV_angle", 1, true);
+  distancePub_ = nh_.advertise<std_msgs::Float64>("distance", 1, true);
 }
 void callback(const geometry_msgs::Point::ConstPtr& pointData)
 {
+
   personpose_.pose.position.x = pointData->x;
   personpose_.pose.position.y = 0;
   personpose_.pose.position.z = pointData->z;
   personpose_.pose.orientation.w = 1;
   personpose_.header.stamp = ros::Time(0);
   personpose_.header.frame_id = "person_detection_frame";
+
 
   listener_.waitForTransform("map","person_detection_frame",ros::Time::now(),ros::Duration(3.0));
   listener_.transformPose("map", personpose_, mappose_);
@@ -35,6 +38,7 @@ void callback(const geometry_msgs::Point::ConstPtr& pointData)
   path_.header.frame_id = "map";
   posestamped_.header.stamp = ros::Time::now();
   posestamped_.header.frame_id = "map";
+  posestamped_.header.seq = seq;
   posestamped_.pose.position = mappose_.pose.position;
   posestamped_.pose.orientation = mappose_.pose.orientation;
   path_.poses.push_back(posestamped_);
@@ -42,6 +46,8 @@ void callback(const geometry_msgs::Point::ConstPtr& pointData)
   move_base_pub_.publish(posestamped_.pose.position);
   FOV_angle.data  = -std::atan2(pointData->z, pointData->x) * 180 / 3.141592 +90;
   anglePub_.publish(FOV_angle);
+  seq++;
+
 }
 
 private:
@@ -50,7 +56,7 @@ private:
   ros::Publisher path_pub_;
   ros::Publisher move_base_pub_;
   ros::Publisher anglePub_;
-
+  ros::Publisher distancePub_;
   tf::TransformBroadcaster br_;
   tf::Transform transform_;
   tf::TransformListener listener_;
@@ -60,6 +66,7 @@ private:
   geometry_msgs::PoseStamped posestamped_;
   std_msgs::Float64 FOV_angle;
   nav_msgs::Path path_;
+  int seq = 0;
 };
 
 int main(int argc, char **argv)
